80a90a60c2aaadc9a8da96b4d6a18a6afde44c1e
Tomek Podsiadlik <podsiadlik.tomek@gmail.com>
2018-09-25 17:46:51 +0200

    api requests functionality started

--- js/app.js
+++ js/app.js
@@ -1,9 +1,44 @@
 const option1 = document.querySelector('.search-option--1');
-const grid = document.querySelector('.grid-container');
+const searchGrid = document.querySelector('.search-grid-container');
 const searchHeader = document.querySelector('.search-header');
+const infoGrid = document.querySelector('.info-grid-container');
 
-option1.addEventListener('click', () => {
-    console.log('to dziala');
+// ------------Shift search options to the top and hide the header------------
+const slideSearchBar = () => {
     searchHeader.classList.add('hide');
-    grid.classList.add('topBar');
-});
+    searchGrid.classList.add('topBar');
+};
+
+// ---------------Display information for specfic search result---------------
+const displayResults = () => {
+
+};
+
+option1.addEventListener('click', getResults );
+
+
+// ---------------------------Get results from SWAPI---------------------------
+async function getResults() {
+    try {
+        console.log('it works1');
+        slideSearchBar();
+        console.log('it works2');
+        const result = await axios('https://swapi.co/api/people/1/');
+        console.log('it works3');
+        console.log(result.data);
+        const arrayKeys = Object.keys(result.data);
+        console.log(arrayKeys);
+        console.log(arrayKeys[0]);
+
+        for (let i = 0 ; i < arrayKeys.length ; i++) {
+            let key = arrayKeys[i];
+
+            console.log(result.data.key);
+            console.log(arrayKeys[i]);
+        }
+        
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
